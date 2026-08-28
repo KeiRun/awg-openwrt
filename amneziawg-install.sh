84a4dd3e9ec0bc676d47493c7257a5c627c7a5a9
KeiRun <50458536+KeiRun@users.noreply.github.com>
2026-08-28 10:53:54 +0700

    Update amneziawg-install.sh

--- amneziawg-install.sh
+++ amneziawg-install.sh
@@ -133,30 +133,11 @@ install_awg_packages() {
     SUBTARGET=$(ubus call system board | jsonfilter -e '@.release.target' | cut -d '/' -f 2)
     VERSION=$(ubus call system board | jsonfilter -e '@.release.version')
     PKGPOSTFIX_BASE="_v${VERSION}_${PKGARCH}_${TARGET}_${SUBTARGET}"
-    BASE_URL="https://github.com/koshelevnv/awg-openwrt/releases/download/"
+    BASE_URL="https://github.com/KeiRun/awg-openwrt/releases/download/"
 
-    # Определяем версию AWG протокола (2.0 для OpenWRT >= 23.05.6 и >= 24.10.3)
-    AWG_VERSION="1.0"
-    MAJOR_VERSION=$(echo "$VERSION" | cut -d '.' -f 1)
-    MINOR_VERSION=$(echo "$VERSION" | cut -d '.' -f 2)
-    PATCH_VERSION=$(echo "$VERSION" | cut -d '.' -f 3)
-
-    if [ "$MAJOR_VERSION" -gt 24 ] || \
-       [ "$MAJOR_VERSION" -eq 24 -a "$MINOR_VERSION" -gt 10 ] || \
-       [ "$MAJOR_VERSION" -eq 24 -a "$MINOR_VERSION" -eq 10 -a "$PATCH_VERSION" -ge 3 ] || \
-       [ "$MAJOR_VERSION" -eq 23 -a "$MINOR_VERSION" -eq 5 -a "$PATCH_VERSION" -ge 6 ]; then
-        AWG_VERSION="2.0"
-        LUCI_PACKAGE_NAME="luci-proto-amneziawg"
-    else
-        LUCI_PACKAGE_NAME="luci-app-amneziawg"
-    fi
-
-
-    # В этом форке собираются пакеты AWG 3.1. Ветка выше нужна только для выбора
-    # имени LuCI-пакета (luci-proto-amneziawg против старого luci-app-amneziawg).
-    if [ "$AWG_VERSION" = "2.0" ]; then
-        AWG_VERSION="3.1"
-    fi
+    # В этом форке собираются пакеты AmneziaWG 3.1
+    AWG_VERSION="3.1"
+    LUCI_PACKAGE_NAME="luci-proto-amneziawg"
 
     printf "\033[32;1mDetected AWG version: $AWG_VERSION\033[0m\n"
 
@@ -227,8 +208,8 @@ install_awg_packages() {
         fi
     fi
 
-    # Устанавливаем русскую локализацию только для AWG 2.0
-    if [ "$AWG_VERSION" = "2.0" ] && [ $ASK_FOR_TRANSLATION = 1 ]; then
+    # Устанавливаем русскую локализацию
+    if [ $ASK_FOR_TRANSLATION = 1 ]; then
         printf "\033[32;1mУстанавливаем пакет с русской локализацией? Install Russian language pack? (y/n) [n]: \033[0m\n"
         read INSTALL_RU_LANG
         INSTALL_RU_LANG=${INSTALL_RU_LANG:-n}
@@ -295,8 +276,8 @@ configure_amneziawg_interface() {
     read -r -p "Enter H3 value (from [Interface]):"$'\n' AWG_H3
     read -r -p "Enter H4 value (from [Interface]):"$'\n' AWG_H4
 
-    # AWG 2.0 новые параметры
-    if [ "$AWG_VERSION" = "2.0" ]; then
+    # AWG 2.0/3.1 дополнительные параметры обфускации
+    if [ "$AWG_VERSION" = "2.0" ] || [ "$AWG_VERSION" = "3.1" ]; then
         read -r -p "Enter S3 value (from [Interface]) [optional, leave blank to skip]:"$'\n' AWG_S3
         read -r -p "Enter S4 value (from [Interface]) [optional, leave blank to skip]:"$'\n' AWG_S4
         read -r -p "Enter I1 value (from [Interface]) [optional, leave blank to skip]:"$'\n' AWG_I1
@@ -322,8 +303,8 @@ configure_amneziawg_interface() {
     uci set network.${INTERFACE_NAME}.awg_h3=$AWG_H3
     uci set network.${INTERFACE_NAME}.awg_h4=$AWG_H4
 
-    # Устанавливаем новые параметры для AWG 2.0 (только если они заданы)
-    if [ "$AWG_VERSION" = "2.0" ]; then
+    # Устанавливаем параметры для AWG 2.0/3.1 (только если они заданы)
+    if [ "$AWG_VERSION" = "2.0" ] || [ "$AWG_VERSION" = "3.1" ]; then
         [ -n "$AWG_S3" ] && uci set network.${INTERFACE_NAME}.awg_s3=$AWG_S3
         [ -n "$AWG_S4" ] && uci set network.${INTERFACE_NAME}.awg_s4=$AWG_S4
         [ -n "$AWG_I1" ] && uci set network.${INTERFACE_NAME}.awg_i1=$AWG_I1
